4a04163e1869d69c81412f02bb30a0ddacdc3008
Enrico Martini <enrico.martini_02@studenti.univr.it>
2019-12-06 19:45:10 +0100

    implementata p_call e p_put, manca sistemare montecarlo

--- src/brownian_motion.py
+++ src/brownian_motion.py
@@ -4,12 +4,10 @@ import math
 from statistics import mean,variance,stdev
 import matplotlib.pyplot as plt
 import numpy as np
-
+from math import exp, log, sqrt, pi
 
 # File csv da salvare nella cartella data per poter essere analizzato
-file_name = 'AAPL.csv'
-
-def getRendimento():
+def analyze(file_name):
     close_adj = []
     with open('../data/' + file_name) as csv_file:
         csv_reader = csv.reader(csv_file,delimiter=',')
@@ -23,27 +21,98 @@ def getRendimento():
         rendimento.append(float(close_adj[i]) / float(close_adj[i-1]) -1)
 
     # Calcolo del mu annuo
-    mu_annuo = []
+    rendimento_annuo = []
     for r in rendimento:
-        mu_annuo.append(r * 252)
-
+        rendimento_annuo.append(r * 252)
     # Calcolo di media e varianza
-    media = mean(mu_annuo)
-    deviation = stdev(mu_annuo)
+    media = mean(rendimento_annuo)
+    deviation = stdev(rendimento_annuo)
+    var = variance(rendimento_annuo)
 
-    return [media,deviation]
+    return [media,deviation,var]
 
-def moto_browniano_geometrico():
-    T = 2
-    mu = 0.1
-    sigma = 0.01
-    S0 = 20
-    dt = 0.01
-    N = round(T/dt)
-    t = np.linspace(0, T, N)
-    W = np.random.standard_normal(size = N) 
-    W = np.cumsum(W)*np.sqrt(dt) ### standard brownian motion ###
-    X = (mu-0.5*sigma**2)*t + sigma*W 
-    S = S0*np.exp(X) ### geometric brownian motion ###
-    plt.plot(t, S)
+def display_GMB(mu,sigma):
+ 
+    S0 = 100 #initial stock price
+    K = 100 #strike price
+    r = mu  # 0.05 #risk-free interest rate
+    #sigma = 0.50 #volatility in market
+    T = 1 #time in years
+    N = 100 #number of steps within each simulation
+    deltat = T/N #time step
+    i = 1000 #number of simulations
+    discount_factor = np.exp(-r*T) #discount factor
+
+    S = np.zeros([i,N])
+    t = range(0,N,1)
+
+    for y in range(0,i-1):
+        S[y,0]=S0
+        for x in range(0,N-1):
+            S[y,x+1] = S[y,x]*(1 + r*deltat + sigma*np.random.normal(0,deltat))
+        plt.plot(t,S[y])
+
+    # Display Geometric Method Brownian Simulation
+    plt.title('Simulations %d Steps %d Sigma %.2f r %.2f S0 %.2f' % (i, N, sigma, r, S0))
+    plt.xlabel('Steps')
+    plt.ylabel('Stock Price')
     plt.show()
+
+def GMB(S0, K, r, v, T):
+
+    N = 100 #number of steps within each simulation
+    deltat = T/N #time step
+    
+    i = 1000 #number of simulations
+    discount_factor = np.exp(-r*T) #discount factor
+
+    S = np.zeros([i,N])
+
+    for y in range(0,i-1):
+        S[y,0]=S0
+        for x in range(0,N-1):
+            S[y,x+1] = S[y,x]*(1 + r*deltat + v*np.random.normal(0,deltat))
+
+    C = np.zeros((i-1,1), dtype=np.float16)
+    for y in range(0,i-1):
+        C[y]=np.maximum(S[y,N-1]-K,0)
+
+    CallPayoffAverage = np.average(C)
+    CallPayoff = discount_factor*CallPayoffAverage
+    return CallPayoff
+
+# Standard normal probability density function
+def norm_pdf(x):
+    return (1.0/((2*pi)**0.5))*exp(-0.5*x*x)
+
+# An approximation to the cumulative distribution function for the standard normal distribution:
+def norm_cdf(x):
+    k = 1.0/(1.0+0.2316419*x)
+    k_sum = k * (0.319381530 + k * (-0.356563782 + \
+        k * (1.781477937 + k * (-1.821255978 + 1.330274429 * k))))
+
+    if x >= 0.0:
+        return (1.0 - (1.0 / ((2 * pi)**0.5)) * exp(-0.5 * x * x) * k_sum)
+    else:
+        return 1.0 - norm_cdf(-x)
+
+def d_j(j, S, K, r, v, T):
+    return (log(S/K) + (r + ((-1)**(j-1))*0.5*v*v)*T)/(v*(T**0.5))
+
+def vanilla_call_price(S, K, r, v, T):
+    """
+    Price of a European call option struck at K, with
+    spot S, constant rate r, constant vol v (over the
+    life of the option) and time to maturity T
+    """
+    return S * norm_cdf(d_j(1, S, K, r, v, T)) - \
+        K*exp(-r*T) * norm_cdf(d_j(2, S, K, r, v, T))
+
+def vanilla_put_price(S, K, r, v, T):
+    """
+    Price of a European put option struck at K, with
+    spot S, constant rate r, constant vol v (over the
+    life of the option) and time to maturity T
+    """
+    return -S * norm_cdf(-d_j(1, S, K, r, v, T)) + \
+        K*exp(-r*T) * norm_cdf(-d_j(2, S, K, r, v, T))
--- src/main.py
+++ src/main.py
@@ -1,8 +1,33 @@
-import brownian_motion
-
+from brownian_motion import analyze, GMB, display_GMB,vanilla_call_price,vanilla_put_price
 
 # Main function
 if __name__ == "__main__":
-    example = brownian_motion.getRendimento()
-    print('media: ' + str(example[0]) + '\n' 'varianza: ' + str(example[1]) )
-    brownian_motion.moto_browniano_geometrico()
+    
+    # Get mean and stdev
+    data = analyze('AAPL.csv')
+    mean = data[0]
+    stdev = data[1]
+    variance = data[2]
+
+    # Simulation variables
+    S = 100         # initial stock price
+    K = 100         # strike price
+    r = mean        # risk-free interest rate
+    v = stdev       # volatility in market
+    T = 1           # time in years
+    N = 100         # number of steps within each simulation
+    deltat = T/N    # time step
+    
+
+    # Draw Geometric Method Brownian Simulation
+    #display_GMB(mean,stdev)
+
+
+    CallPayoff = GMB(S,K,r,v,T)
+
+    call = vanilla_call_price(S,K,r,v,deltat)
+    put = vanilla_put_price(S,K,r,v,deltat)
+    
+    print('CallPayoff: ' + str(CallPayoff) + '\n'\
+          'Call:       ' + str(call) + '\n'\
+          'Put:        ' + str(put) )
